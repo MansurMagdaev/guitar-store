1cd8747aef94c4b59dd677d88cfe67590a0ad6e6
Mans <magdaevmansur@mail.com>
2023-11-26 18:41:05 +0300

    restoration of performance

--- index.js
+++ index.js
@@ -5,22 +5,6 @@ function render() {
     productsPage.render();
 
 }
-spinnerPage.render();
-let CATALOG = [];
 
-fetch('server/catalog.json')
-    .then(res => res.json()) 
-    .then(body => {
-        CATALOG = body;
-    
-        setTimeout(() => {
-            spinnerPage.handleClear();
-            render();  
-        }, 1000);
 
-})
-    .catch(error => {
-    spinnerPage.handleClear();
-    errorPage.render();
-})
-
+render()
